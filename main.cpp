56ded21415a98e3828e21ff72bebd7a11f746f3b
yosiawan <yosiasetiawan@outlook.com>
2024-06-07 23:37:09 +0700

    session 4 thread 1

--- main.cpp
+++ main.cpp
@@ -2,20 +2,44 @@
 // Created by Yosia on 07/06/24.
 //
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    float distance, time;
+struct Student {
+    std::string name{};
+    int result{};
+};
 
-    printf("Enter distance traveled: ");
-    std::cin >> distance;
+int main() {
+    int numOfStudents;
+    std::cout << "Masukkan jumlah mahasiswa: ";
+    std::cin >> numOfStudents;
 
-    printf("Enter time elapsed: ");
-    std::cin >> time;
+    std::vector<Student> students(numOfStudents);
 
-    const float speed = distance / time;
+    for (int studentNum = 0; studentNum < numOfStudents; ++studentNum) {
+        printf("Masukkan Nama Mahasiswa ke %d:", studentNum + 1);
+        std::cin >> students[studentNum].name;
 
-    printf("Your speed is %.f", speed);
+        printf("Masukkan Nilai Mahasiswa ke %d:", studentNum  + 1);
+        std::cin >> students[studentNum].result;
+    }
+
+    int numOfPassingStudents = 0;
+    for (auto & student : students) {
+        printf("Nama: %s.     Hasil Ujian: %s.\n", student.name.c_str(), student.result == 1 ? "LULUS":"GAGAL");
+        if (student.result == 1) {
+            ++numOfPassingStudents;
+        }
+    }
+
+    const float passingRate = static_cast<float>(numOfPassingStudents) / static_cast<float>(numOfStudents);
+    printf("Jumlah mahasiswa yang LULUS: %d.\n"
+           "Jumlah mahasiswa yang GAGAL: %d.\n",
+           numOfPassingStudents,
+           numOfStudents - numOfPassingStudents);
+    if (passingRate >= 0.8) {
+        printf("Kelulusan kelas telah mencapai target!");
+    }
+
     return 0;
-
 }
