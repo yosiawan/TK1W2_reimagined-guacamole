f3e5e43fe2821825963e424c3e1a459e84827a1f
augierizkyk <augie.rizky@binus.ac.id>
2024-06-09 00:33:55 +0700

    soal no 2 DONE

--- MK_Algo-TugasKelompok1/stringManipulator/stringManipulator.cpp
+++ MK_Algo-TugasKelompok1/stringManipulator/stringManipulator.cpp
@@ -6,44 +6,58 @@
 #include <string.h>
 #include <ctype.h>
 
+void reverseString(char *str) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    // Swap characters until start < end
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
 int stringManipulator() {
-    char kalimat1[100], kalimat2[100], kalimat3[100];
+    char kalimat1[100], kalimat2[100], kalimat3[100], sentence[100];
 
     printf("Kalimat 1 = ");
     fgets(kalimat1, sizeof(kalimat1), stdin);
     kalimat1[strcspn(kalimat1, "\n")] = 0;
 
-
     printf("Kalimat 2 = ");
     fgets(kalimat2, sizeof(kalimat2), stdin);
     kalimat2[strcspn(kalimat2, "\n")] = 0;
-
 
     printf("Kalimat 3 = ");
     fgets(kalimat3, sizeof(kalimat3), stdin);
     kalimat3[strcspn(kalimat3, "\n")] = 0;
 
-    int jumlah1 = 0, jumlah2 = 0, jumlah3 = 0;
-    for (int i = 0; kalimat1[i] != '\0'; i++) {
-        if (!isspace(kalimat1[i])) {
-            jumlah1++;
-        }
-    }
-    for (int i = 0; kalimat2[i] != '\0'; i++) {
-        if (!isspace(kalimat2[i])) {
-            jumlah2++;
-        }
-    }
-    for (int i = 0; kalimat3[i] != '\0'; i++) {
-        if (!isspace(kalimat3[i])) {
-            jumlah3++;
-        }
-    }
+    // Combining the strings into a single variable
+    sprintf(sentence, "%s %s %s", kalimat1, kalimat2, kalimat3);
+
+    printf("Gabungan 3 buah inputan kalimat = %s \n", sentence);
+
+    // Reverse each individual word
+    reverseString(kalimat1);
+    reverseString(kalimat2);
+    reverseString(kalimat3);
+
+    // Combine reversed words
+    printf("Kalimat setelah dibalik = %s %s %s\n", kalimat3, kalimat2, kalimat1);
+
+    int jumlah1 = strlen(kalimat1);
+    int jumlah2 = strlen(kalimat2);
+    int jumlah3 = strlen(kalimat3);
     int total = jumlah1 + jumlah2 + jumlah3;
 
+    printf("Jumlah Huruf kalimat Pertama = %d\n", jumlah1);
     printf("Jumlah Huruf kalimat kedua = %d\n", jumlah2);
     printf("Jumlah Huruf kalimat ketiga = %d\n", jumlah3);
     printf("Total Jumlah huruf = %d\n", total);
 
     return 0;
-};
+}
